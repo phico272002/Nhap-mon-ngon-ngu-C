c52a18c9b38cc190ea4e77fd49ea1419ff66543c
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-11-26 10:43:18 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -2,14 +2,25 @@
 
 int main(void)
 {
-	// khai boa bien num va khoi tao bien a va b
-	int a = 3;
-	int b = 5;
-	printf("Tinh Tong A+B = %d\n",a+b);
-	printf("Tinh Hieu A-B = %d\n",a-b);
-	printf("Tinh Tich A*B = %d\n",a*b);
-	printf("Tinh Thuong A/B = %d\n",a/b);
-	printf("Tinh Thuong: %d / %d = %f",a,b,(float)a/b);
+	// Khai bao bien a, b kieu int
+	int a,b;
+	// khoi tao bien a = 3
+	a = 3;
+	// khoi tao bien b = 5
+	b = 5;
+	// Khai bao cac bien tong, hieu, tich
+	int tong, hieu, tich;
+	// Khai bao bien thuong
+	double thuong;
+	// gan tong a + b
+	tong = a + b;
+	// gan hieu a - b
+	hieu = a - b;
+	// gan tich a * b
+	tich = a * b;
+	// gan thuong a / b va ep kieu
+	thuong = (double)a / b;
+	printf("a + b = %d, a - b = %d, a * b = %d, a / b = %0.1f", tong, hieu, tich, thuong);
 	getchar();
 	return 0;
 	
