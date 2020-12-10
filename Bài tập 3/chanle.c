8aed9682706df2616bc05c99549d096f2ae5309d
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-12-10 09:08:30 +0700

    Add files via upload

---
+++ Bài tập 3/chanle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void)
+{
+	//Khai bao bien a
+	int a;
+	//Hien thi ra man hinh
+	printf("Nhap so \n");
+	//Nhap mot so ra tu ban phim
+	scanf("%d",&a);
+	//Khiem tra a la duong hay am
+	if(a % 2==0 ) {
+		printf("%d la so chan ",a);
+		} else if (a % 2==0) {
+		printf("a = 0");
+		}
+		else {
+		printf("%d la so le",a);
+		}
+
+getchar();
+return 0;
+}
