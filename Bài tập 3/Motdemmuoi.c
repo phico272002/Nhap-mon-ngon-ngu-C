748a0e87a88ebfade9cdc2a9c2a86eb79bd64385
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-12-24 08:38:21 +0700

    Update Motdemmuoi.c

--- Bài tập 3/Motdemmuoi.c
+++ Bài tập 3/Motdemmuoi.c
@@ -4,11 +4,15 @@ int main(void)
 {
 	//Khai bao bien
 	int i;
-	//Vong lap tu 1 den 10
+	//Vong lap tu 1 den 10 tren 1 hang
 	for (i = 1; i < 11; ++i)
 {
-	//Hien thi ra man hinh 
-	printf("\t %d \n", i);
+		printf("\t %d ", i);
 }
+	for (i = 1; i < 11; ++i)
+{
+		printf(" \t \n %d ",i);
+}
+
 return 0;
 }
