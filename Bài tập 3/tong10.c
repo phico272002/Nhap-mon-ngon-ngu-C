d6f03af0a9bc65e7d40eecc3253f1ac7f626f0cf
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-12-24 10:48:26 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 main()
-{
+
 {
 	//Khai bao bien
-int i, j;
-	//Hien thi ra man hinh 
-	printf("Tinh tong gia tri tu 1 den 10 ");
+int tong;
+	
+	tong = 0;
+int dem;
 	//Vong lap for tinh tong
-for (i = 1, j = 0; i <= 10; i= i +1)
-	//Hien thi ra man hinh
-	printf("\n %d + %d = %d", i, j, i + j);
+for (dem = 1; dem <= 10; dem++) 
+{
+	//Tinh tong
+	tong = tong + dem;
 }
-
+	//Hien thi ra man hinh
+	printf("Tong la: %d", tong);
 return 0;
 }
