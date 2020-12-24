1a197a9d804ab8475749132c67d987a4639aec25
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-12-24 08:29:34 +0700

    Add files via upload

---
+++ Bài tập 3/Motdemmuoi.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+
+int main(void) 
+{
+	//Khai bao bien
+	int i;
+	//Vong lap tu 1 den 10
+	for (i = 1; i < 11; ++i)
+{
+	//Hien thi ra man hinh 
+	printf("\t %d \n", i);
+}
+return 0;
+}
