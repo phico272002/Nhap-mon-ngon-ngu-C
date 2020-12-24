7d9fc952e17495e9310fc8909c7253ca6bc5078f
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2020-12-24 10:31:31 +0700

    Add files via upload

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main()
+{
+{
+	//Khai bao bien
+int i, j;
+	//Hien thi ra man hinh 
+	printf("Tinh tong gia tri tu 1 den 10 ");
+	//Vong lap for tinh tong
+for (i = 1, j = 0; i <= 10; i= i +1)
+	//Hien thi ra man hinh
+	printf("\n %d + %d = %d", i, j, i + j);
+}
+
+return 0;
+}
