0cb6c785a39f65f74a19ac0a65d5f45ad480e5ba
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-01-07 09:28:47 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -1,52 +1,85 @@
 #include <stdio.h>
-int main()
 
+
+int main() 
 {
-	//Khai bao bien
-int n;
-	//Nhap so tu ban phim
-	scanf("%d", &n);
-	//Nhap 1 de ra one
-if (n <= 1 )
+    //Khai bao bien a, b, i
+    int a, b, i;
+    //Nhap so lieu tu ban phim
+    scanf("%d", &a);
+    //Hien thi ket qua ra man hinh
+    scanf("%d", &b);
+    //Vong lap for
+for(i = a; i <= b; i++)
+
+    //Dieu kien 1 be hon hoac bang i va i be hon hoac bang 9
+if(1 <= i && i <= 9)
 {
-	//Hien thi ra man hinh
-	printf("one");
-}	
-else if (n <= 2)
-{	
-	printf("two");
+    //i be hon hoac bang 1
+if(i <= 1)
+{
+    //Hien thi ket qua ra man hinh la one
+    printf("one\n");
+}    //i be hon hoac bang 2
+else if(i <= 2)
+{
+    //Hien thi ket qua ra man hinh la Two
+    printf("two\n");
 }
-else if (n <= 3)
+    //i be hon hoac bang 3 
+else if(i <= 3)
 {
-	printf("three");
+        //Hien thi ket qua ra man hinh la Three
+    printf("three\n");
 }
-else if (n <= 4)
+    //i be hon hoac bang 4 
+else if(i <= 4)
 {
-	printf("four");
+        //Hien thi ket qua ra man hinh la Four
+    printf("four\n");
 }
-else if (n <= 5)
+    //i be hon hoac bang 5 
+else if(i <= 5)
+{    
+    //Hien thi ket qua ra man hinh la Five
+    printf("five\n");
+}
+    //i be hon hoac bang 6 
+else if(i <= 6)
 {
-	printf("five");
+        //Hien thi ket qua ra man hinh la Six
+    printf("six\n");
 }
-else if (n <= 6)
+    //i be hon hoac bang 7
+else if(i <= 7)
 {
-	printf("six");
+        //Hien thi ket qua ra man hinh la Seven
+    printf("seven\n");
 }
-else if (n <= 7)
+    //i be hon hoac bang8
+else if(i <= 8)
 {
-	printf("seven");
+        //Hien thi ket qua ra man hinh la Eight
+    printf("eight\n");
 }
-else if (n <= 8)
+    //i be hon hoac bang 9
+else{
+        //Hien thi ket qua ra man hinh la Nine
+    printf("nine\n");
+}
+}
+else
 {
-	printf("eight");
-}
-else if (n <= 9)
+if(i % 2 == 0)
 {
-	printf("nine");
+     printf("even\n");
 }
-else if (n > 9)
+else
 {
-	printf("Greater than 9",n);
+     printf("odd\n");
 }
+}
+
+
 return 0;
 }
