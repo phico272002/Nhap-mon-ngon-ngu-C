e6628d3a93e62db26b6fb509a2a65bdb15a93ff7
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-01-07 10:28:53 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -5,18 +5,20 @@ int main()
   //Khai bao bien i va j
 	int tich1 = 2, tich2 =3, tich3 = 4;
 		printf("\tBANG CUU CHUONG\n");
-    	printf("2x1=%d\t",tich1 * 1);
-    	printf("2x2=%d\t",tich1 * 2);
-    	printf("2x3=%d\t",tich1 * 3 );
+    	printf("2x1=%d\t\n",tich1 * 1);
+    	printf("2x2=%d\t\n",tich1 * 2);
+    	printf("2x3=%d\t\n",tich1 * 3 );
     	printf("2x4=%d\t\n",tich1 * 4);
-    	printf("3x1=%d\t",tich2 * 1);
-    	printf("3x2=%d\t",tich2 * 2);
-    	printf("3x3=%d\t",tich2 * 3);
+    	printf("\nBANG CUU CHUONG");
+    	printf("3x1=%d\t\n",tich2 * 1);
+    	printf("3x2=%d\t\n",tich2 * 2);
+    	printf("3x3=%d\t\n",tich2 * 3);
     	printf("3x4=%d\t\n",tich2 * 4);
-    	printf("4x1=%d\t",tich3 * 1);
-   		printf("4x2=%d\t",tich3 * 2);
-   		printf("4x3=%d\t",tich3 * 3);
-   		printf("4x4=%d\t",tich3 * 4);
+    	printf("\nBANG CUU CHUONG");
+    	printf("4x1=%d\t\n",tich3 * 1);
+   		printf("4x2=%d\t\n",tich3 * 2);
+   		printf("4x3=%d\t\n",tich3 * 3);
+   		printf("4x4=%d\t\n",tich3 * 4);
   
 return 0;
 }
