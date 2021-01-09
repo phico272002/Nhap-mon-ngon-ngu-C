28909ae37a5054a9f69809c881af015150e7bb44
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-01-09 10:10:14 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -1,51 +1,22 @@
 #include <stdio.h>
 
+
 int main()
 {
-        //Khai bao bien kieu int
-  int i, j;
-  int h = 4;
-      //Hien thi ra man hinh tam giac can dac
-      printf("\t                  Tam giac can dac \n");
-  if(h > 0)
-    { // cau a
-  for(i = 1; i <= h; i = i + 1)
-    {
-  for(j = 1; j < h * 2; j = j + 1)
-    {
-  if(abs(h - j)<= (i - 1))
-          {
-          printf("\t * ");
-          }
-         else
-          {
-          printf("\t ");
-          }
-    }
-          printf("\n");
-    }
-    }
-  int k, a, b ;
-  k = h -1 ;
-      {
-        printf ("\t                  Tam giac can rong \n");
-      }
-  for(a = 0; a < h - 1; a = a + 1)
-    {// cau b
-      for(b = 0; b < 2 * h - 1; b = b + 1)
-      {
-        if(b == k - a || b == k + a)
-          {
-           printf("\t * ");
-          }
-         else
-          {
-          printf("\t ");
-          }
-      }
-          printf("\t \n");
-    }
-    for(a = 0; a < 2 * h - 1; a = a + 1)
-          printf("\t * ");
+  //h = 4 : 4 hang
+  //Hang 1: 1 ngoi sao , 6 khoang trong
+  //Hang 2: 3 ngoi sao , 4 khoang trong  
+  //Hang 3: 5 ngoi sao , 2 khoang trong
+  //Hang 4: 7 ngoi sao , 0 khoang trong
+  //h = 3 : 3 hang
+  //Hang 1: 1 ngoi sao , 4 khoang trong
+  //Hang 2: 3 ngoi sao , 2 khoang trong
+  //Hang 3: 5 ngoi sao , 0 khoang trong
+  //h = 2 : 2 hang
+  //Hang 1: 1 ngoi sao , 2 khoang trong
+  //Hang 2: 3 ngoi sao , 0 khoang trong  
+  //h = 1 : 1 hang
+  //Hang 1: 1 ngoi sao, 6 khoang trong
+  
 return 0;
 }
