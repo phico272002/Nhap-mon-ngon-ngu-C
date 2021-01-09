9fdb20178d40ffc3c7f2189845997b76d047f6ce
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-01-09 10:27:07 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -4,19 +4,19 @@
 int main()
 {
   //h = 4 : 4 hang
-  //Hang 1: 1 ngoi sao , 6 khoang trong
-  //Hang 2: 3 ngoi sao , 4 khoang trong  
-  //Hang 3: 5 ngoi sao , 2 khoang trong
+  //Hang 1: 1 ngoi sao , 3 khoang trong
+  //Hang 2: 3 ngoi sao , 2 khoang trong  
+  //Hang 3: 5 ngoi sao , 1 khoang trong
   //Hang 4: 7 ngoi sao , 0 khoang trong
   //h = 3 : 3 hang
-  //Hang 1: 1 ngoi sao , 4 khoang trong
-  //Hang 2: 3 ngoi sao , 2 khoang trong
+  //Hang 1: 1 ngoi sao , 2 khoang trong
+  //Hang 2: 3 ngoi sao , 1 khoang trong
   //Hang 3: 5 ngoi sao , 0 khoang trong
   //h = 2 : 2 hang
-  //Hang 1: 1 ngoi sao , 2 khoang trong
+  //Hang 1: 1 ngoi sao , 1 khoang trong
   //Hang 2: 3 ngoi sao , 0 khoang trong  
   //h = 1 : 1 hang
-  //Hang 1: 1 ngoi sao, 6 khoang trong
+  //Hang 1: 1 ngoi sao, 3 khoang trong
   
 return 0;
 }
