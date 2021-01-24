e91d8f123171918369c27cd6ff79167b50ddb0fd
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-01-24 15:05:02 +0700

    Create ham_03.c

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,41 @@
+//****************************************CODE**********
+//Khoi khai bao
+#include<stdio.h>
+void Nhap_pt_mang(int a[])
+{
+  Nhap_pt_mang(a);
+
+}
+void Hien_thi_pt_mang(int a[])
+{
+   Hien_thi_pt_mang(a);
+  
+}
+void Hien_thi_tong_chan(int a[])
+{
+ Hien_thi_tong_chan(a);
+}
+void Dem_chan_le_0(int a[])
+{
+  Dem_chan_le_0(a);
+}
+
+ //khoi ham main
+int main(){
+  int n;
+  printf("Nhap n = ");
+  scanf("%d", &n);
+  int a[n];
+  Nhap_pt_mang(a);
+  Hien_thi_pt_mang(a);  
+  Hien_thi_tong_chan(a);
+  Dem_chan_le_0(a);
+  return 0;
+}
+
+ 
+
+
+// khoi dinh nghia ham
+
+//*************************END CODE*****************************
