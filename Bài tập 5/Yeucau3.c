859166bc25236fb85401ed4cc51923edcdc08143
Nguyễn Phi Cơ <72533359+phico272002@users.noreply.github.com>
2021-03-20 08:21:41 +0700

    Create Yeucau3.c

---
+++ Bài tập 5/Yeucau3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+ //Tim uoc chung lon nhat
+int UCLN(int a, int b) {
+    if (b == 0) return a;
+    return UCLN(b, a % b);
+}
+int main() 
+{
+ int a, b;
+    printf("Nhap a = ");
+    scanf("%d", &a);
+    printf("Nhap b = ");
+    scanf("%d", &b);
+    // tinh USCLN cua a va b
+    printf("UCLN cua %d va %d la: %d", a, b, UCLN(a, b));
+    
+}
